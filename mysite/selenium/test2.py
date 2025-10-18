110575c609b8cce3bb6253accc054b1e5b3f5eb5
r-22201030 <22201030uap-bd.edu>
2025-10-18 15:40:18 +0600

    testing

---
+++ mysite/selenium/test2.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver import Keys
+
+# Initialize Chrome WebDriver
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
+# Open Google
+driver.get("http://127.0.0.1:8000/login/")
+
+# Find the search box by its element ID
+googleSearchBox = driver.find_element(By.ID, value="username")
+
+# Type a search query
+googleSearchBox.send_keys("rtu")
+
+# Wait for a few seconds
+time.sleep(10)
+
+# Close and quit the browser
+driver.close()
+driver.quit()
